72ab5f2fc34be4964fb52d58c89c0eae210a3f34
1000ch <shogo.sensui@gmail.com>
2019-07-13 23:33:30 +0900

    refactor: update build env

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,32 @@
-import babel from 'rollup-plugin-babel'
-import vue from 'rollup-plugin-vue'
+import pkg from './package.json';
+import commonjs from 'rollup-plugin-commonjs';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import vue from 'rollup-plugin-vue';
+import { terser } from 'rollup-plugin-terser'
 
 export default {
   input: 'src/index.js',
-  output: {
-    file: 'dist/index.js',
+  output: [{
+    file: `dist/${pkg.name}.common.js`,
     format: 'cjs'
-  },
+  }, {
+    file: `dist/${pkg.name}.esm.js`,
+    format: 'esm',
+  }, {
+    file: `dist/${pkg.name}.js`,
+    format: 'umd',
+    name: 'VueGrd'
+  }],
   plugins: [
+    nodeResolve(),
+    commonjs(),
     vue({
       css: true
     }),
     babel({
-      exclude: 'node_modules/**'
-    })
+      extensions: ['.js', '.vue']
+    }),
+    terser()
   ]
-}
+};
