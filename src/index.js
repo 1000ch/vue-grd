d78a77cef32001e0414f96fa8542a807d56a7cd6
1000ch <shogo.sensui@gmail.com>
2019-07-24 22:22:03 +0900

    feat: enable install

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import VueGrid from './VueGrid.vue';
 import VueCell from './VueCell.vue';
 
+function install(Vue, options) {
+  Vue.component('vue-grid', VueGrid);
+  Vue.component('vue-cell', VueCell);
+}
+
 export default {
+  install,
   VueGrid,
   VueCell
 };
