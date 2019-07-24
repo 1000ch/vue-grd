ebad78133f22b7812a896d4c7ef3700b1fa73e79
1000ch <shogo.sensui@gmail.com>
2019-07-24 22:24:05 +0900

    feat: add types

---
+++ index.d.ts
@@ -0,0 +1,50 @@
+import { PluginFunction } from 'vue';
+
+type AlignValue =
+  'top' |
+  'middle' |
+  'bottom' |
+  'stretch' |
+  'baseline';
+
+type JustifyValue =
+  'start' |
+  'center' |
+  'end' |
+  'between' |
+  'around';
+
+type WidthValue =
+  'fill' |
+  '1of12' |
+  '2of12' |
+  '3of12' |
+  '4of12' |
+  '5of12' |
+  '6of12' |
+  '7of12' |
+  '8of12' |
+  '9of12' |
+  '10of12' |
+  '11of12' |
+  '12of12';
+
+export interface VueGrdOptions {}
+
+export interface VueGrid {
+  align: AlignValue;
+  justify: JustifyValue;
+  readonly computedAlign: string;
+  readonly computedJustify: string;
+}
+
+export interface VueCell {
+  width: WidthValue;
+  readonly computedWidth: string;
+}
+
+declare const installer: {
+  install: PluginFunction<VueGrdOptions>;
+};
+
+export default installer;
