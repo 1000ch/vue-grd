9b4e4d347888f9ec3f29269352852972e4274dc9
1000ch <shogosensui@gmail.com>
2021-11-29 16:53:44 +0900

    Various tweaks

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import pkg from './package.json';
-import commonjs from 'rollup-plugin-commonjs';
-import nodeResolve from 'rollup-plugin-node-resolve';
-import babel from 'rollup-plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
 import vue from 'rollup-plugin-vue';
 import { terser } from 'rollup-plugin-terser';
 
